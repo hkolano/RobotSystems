b02007c5e3e2cc56b5a51d892f6eb6066b7705b8
Hannah Kolano <hkolano@comcast.net>
2022-02-24 12:45:03 -0800

    removed logdecorator

--- ArmActions.py
+++ ArmActions.py
@@ -12,7 +12,6 @@ from ArmIK.ArmMoveIK import *
 import HiwonderSDK.Board as Board
 from CameraCalibration.CalibrationConfig import *
 import logging
-from logdecorator import log_on_start , log_on_end , log_on_error
 
 
 if sys.version_info.major == 2:
@@ -27,6 +26,7 @@ class ArmMover():
         self.AK = ArmIK() 
 
     def go_to_initial_position(self):
+        logging.info("Moving arm to initial position.")
         Board.setBusServoPulse(1, self.close_gripper_servo_value - 50, 300)
         Board.setBusServoPulse(2, 500, 500)
         self.AK.setPitchRangeMoving((0, 10, 10), -30, -30, -90, 1500)
