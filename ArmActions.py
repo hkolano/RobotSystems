5101c8d8273a66dbee1cae37bbb8cc446f8d3347
Hannah Kolano <hkolano@comcast.net>
2022-02-24 12:36:45 -0800

    added logging imports

--- ArmActions.py
+++ ArmActions.py
@@ -11,6 +11,9 @@ from ArmIK.Transform import *
 from ArmIK.ArmMoveIK import *
 import HiwonderSDK.Board as Board
 from CameraCalibration.CalibrationConfig import *
+import logging
+from logdecorator import log_on_start , log_on_end , log_on_error
+
 
 if sys.version_info.major == 2:
     print('Please run this program with python3!')
@@ -27,6 +30,7 @@ class ArmMover():
         Board.setBusServoPulse(1, self.close_gripper_servo_value - 50, 300)
         Board.setBusServoPulse(2, 500, 500)
         self.AK.setPitchRangeMoving((0, 10, 10), -30, -30, -90, 1500)
+        
 
 
 if __name__ == "__main__":
