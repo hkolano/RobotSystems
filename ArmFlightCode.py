73af6182c0836300d15770dd46a4a570b769efc3
Hannah Kolano <hkolano@comcast.net>
2022-02-24 17:05:12 -0800

    reallllly long time delay

--- ArmFlightCode.py
+++ ArmFlightCode.py
@@ -35,7 +35,7 @@ class Flight():
             if img is not None:
                 frame = self.p.detect_cubes(img)
                 cv2.imshow('Frame', frame)
-                time.sleep(1)
+                time.sleep(5)
                 _ = self.p.detect_cubes(img)
                 self.sort_next_block()
                 key = cv2.waitKey(1)
